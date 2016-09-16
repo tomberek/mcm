df6436e5d884b5badb40b5de9e47a8d6a58fb16a
Nick Small <nickjsiii@gmail.com>
2016-09-16 16:35:26 -0400

    SAX parser almost works

--- xml_to_md.rb
+++ xml_to_md.rb
@@ -82,17 +82,35 @@ class NodeParser
   end
 end
 
-module SAXOpenNodes
-  def preface(name, attrs)
+module SAXNodes
+  def preface_open(name, attrs)
     @parts[:preface] = @output = []
   end
 
-  def line(name, attrs)
-    @output << '\n\n' unless @output.last == '\n\n'
+  def preface_close(name, attrs)
+    @output = []
+  end
+
+  def line_open(name, attrs)
+    @output << "\n\n" unless @output.last == "\n\n"
+  end
+
+  def italic(string)
+    @output << "_#{string}_"
+  end
+
+  def bold(string)
+    @output << "__#{string}__"
+  end
+
+  def change(string)
+    @output << "___#{string}___"
   end
 end
 
 class MCMDoc < Nokogiri::XML::SAX::Document
+  include SAXNodes
+
   attr_reader :output
   attr_reader :parts
 
@@ -100,21 +118,30 @@ class MCMDoc < Nokogiri::XML::SAX::Document
     super
     @output = []
     @parts = {}
+    @whole_node_cache = []
   end
 
   def start_element(name, attrs=[])
-    method_name = name.downcase.to_sym
-    if SAXOpenNodes.method_defined?(method_name)
-      SAXOpenNodes.send(method_name, name, attrs)
+    method_name = "#{name}_open".downcase.to_sym
+    if SAXNodes.method_defined?(method_name)
+      send(method_name, name, attrs)
     end
+
+    start_whole_node(name, attrs)
   end
 
   def end_element(name, attrs=[])
-    # puts "ending: #{name}"
+    method_name = "#{name}_close".downcase.to_sym
+    if SAXNodes.method_defined?(method_name)
+      send(method_name, name, attrs)
+    end
+
+    end_whole_node(name, attrs)
   end
 
   def characters(string)
-    @output << clean_text(string)
+    puts string if string.include? '&bull;'
+    @output << clean_text(string) unless continue_whole_node(string)
   end
   #
   # def error string
@@ -123,8 +150,36 @@ class MCMDoc < Nokogiri::XML::SAX::Document
 
   def output
     @parts.each do |key, value|
-      File.open("manual/#{key}.md", 'w') { |file| file.write(value) }
+      File.open("manual/#{key}.md", 'w') { |file| file.write(value.join('')) }
     end
+  end
+
+  private
+
+  def start_whole_node(name, attrs)
+    method_name = name.downcase.to_sym
+    if SAXNodes.method_defined?(method_name)
+      @whole_node_cache << {key: method_name, value: []}
+    end
+  end
+
+  def continue_whole_node(string)
+    @whole_node_cache.last&.fetch(:value)&.push(string)
+  end
+
+  def end_whole_node(name, attrs)
+    method_name = name.downcase.to_sym
+    last = @whole_node_cache.last
+    return if !last
+
+    if last[:key] != method_name
+      puts "ERROR"
+      return
+    end
+
+    output = last[:value].join('')
+    @whole_node_cache.pop
+    send(method_name, output)
   end
 end
 
@@ -132,5 +187,4 @@ document = MCMDoc.new
 parser = Nokogiri::XML::SAX::Parser.new(document)
 File.open('mcm_5_jun_2016.xml') { |f| parser.parse(f) { |ctx| ctx.recovery = true } }
 
-# document.output
-puts document.parts
+document.output
